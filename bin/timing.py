8cc7007d722d6c853717ef4303691f9d576e623f
Colin McKechney <cmckechn@nd.edu>
2021-06-12 09:26:36 -0400

    typo

--- bin/timing.py
+++ bin/timing.py
@@ -35,24 +35,26 @@ csvWriter = csv.writer(file,csv.get_dialect('excel'))
 
 i = 1000000
 capacityAttempt = 0
-subprocess.run(['./write'])
+#subprocess.run(['./write'])
 
 while i < 10000000:
     j=0
     while j < 10:
         row = [i]
-        result = subprocess.run(['./homomorphicSum','-n',f'{i}'],stdout=subprocess.PIPE,stderr=subprocess.DEVNULL)
-        row += bytes.decode(result.stdout).strip()
-        result = subprocess.run(['graphene-sgx','decode'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-        if result.returncode > 0:
+        result = subprocess.run(['./homomorphicSum',' -n',f' {i}'],stdout=subprocess.PIPE,stderr=subprocess.DEVNULL)
+        row+= bytes.decode(result.stdout).strip().split('\n')
+        result = subprocess.run(['graphene-sgx',' decode'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+        row+= bytes.decode(result.stdout).strip().split('\n')
+        '''if result.returncode > 0:
             increaseCapacity(capacityAttempt)
             capacityAttempt+=1
             continue
-        row += bytes.decode(result.stdout).strip()
+        bytes.decode(result.stdout).strip().split('\n')
         if capacityAttempt >0:
             csvWriter.writerow(row + [capacities[capacityAttempt-1]])
         else:
-            csvWriter.writerow(row + ["512M"])
+            csvWriter.writerow(row + ["512M"])'''
+        csvWriter.writerow(row)
         j+=1
     i+=1000000
 
